629742ff7d5871bf83e232625248275975f056b1
melroh629 <jw.roh629@gmail.com>
2024-03-08 12:43:12 +0900

    feat: 크롤링 및 기능추가

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const refreshPage = () => {
 }
 buttonRefresh.addEventListener('click', refreshPage);
 
-
 //시스템 날짜 노출
 const getTodayDate = () => {
     const today = new Date();
@@ -18,35 +17,73 @@ const getTodayDate = () => {
     document.getElementById('current-date').textContent = `${year}년 ${month}월 ${date}일 ${weekDay[day]}요일`;
 }
 document.addEventListener('DOMContentLoaded', getTodayDate);
-// 언론사 목록을 동적으로 생성하는 코드
-document.addEventListener('DOMContentLoaded', () => {
-    const pressList = document.getElementById('press-list');
-    const pressData = [
-        { name: '서울경제', imgSrc: 'https://s.pstatic.net/static/newsstand/2019/logo/011.png' },
-        //TODO: 다른 언론사 추가
-    ];
 
-    pressData.forEach(press => {
-        const li = document.createElement('li');
-        const a = document.createElement('a');
-        const img = document.createElement('img');
-        const span = document.createElement('span');
-        const button = document.createElement('button');
-        
-        a.href = '#';
-        img.src = press.imgSrc;
-        img.alt = press.name;
-        span.className = 'subscribe';
-        button.className = 'button-subscribe';
-        button.textContent = '+ 구독하기';
-        
-        a.appendChild(img);
-        span.appendChild(button);
-        li.appendChild(a);
-        li.appendChild(span);
-        pressList.appendChild(li);
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('images.json') 
+        .then(response => response.json())
+        .then(pressData => {
+            const wrapper = document.querySelector('.swiper-wrapper');
+            console.log(pressData);
+            // 각 슬라이드를 만들고 추가
+            let currentSlide;
+            let slideCounter = 0;
+            pressData.forEach((press, index) => {
+                if (index % 24 === 0) {
+                    currentSlide = document.createElement('div');
+                    currentSlide.classList.add('swiper-slide');
+                    wrapper.appendChild(currentSlide);
+                    slideCounter++;
+                }
+                currentSlide.innerHTML += ListComponent(press);
+            });
+            
+            //TODO: 버튼 환성화/비활성화
+            const prevButton = document.querySelector('.prev');
+            prevButton.disabled = true;
+
+            let currentSlideIndex = 0;
+
+            function updateButtonState() {
+                const nextButton = document.querySelector('.next');
+                nextButton.disabled = currentSlideIndex === wrapper.children.length - 1;
+
+                const prevButton = document.querySelector('.prev');
+                prevButton.disabled = currentSlideIndex === 0;
+            }
+
+            updateButtonState();
+
+            document.querySelector('.next').addEventListener('click', () => {
+                if (currentSlideIndex < wrapper.children.length - 1) {
+                    wrapper.children[currentSlideIndex].style.display = 'none';
+                    currentSlideIndex++;
+                    wrapper.children[currentSlideIndex].style.display = 'grid';
+                    updateButtonState();
+                }
+            });
+
+            document.querySelector('.prev').addEventListener('click', () => {
+                if (currentSlideIndex > 0) {
+                    wrapper.children[currentSlideIndex].style.display = 'none';
+                    currentSlideIndex--;
+                    wrapper.children[currentSlideIndex].style.display = 'grid';
+                    updateButtonState();
+                }
+            });
+        });
 });
 
 
-//크롤링
+function ListComponent(press) {
+    return `
+        <div class="swiper-item">
+            <a href="#">
+                <img id="pressLogo" src="${press.src}" alt="${press.alt}">
+            </a>
+            <span class="subscribe">
+                <button class="button-subscribe">+ 구독하기</button>
+            </span>
+        </div>
+        `
+}
+
